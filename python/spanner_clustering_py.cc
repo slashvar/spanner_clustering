bcb9ee207363fc92260c262ad7a0d1ccc639aa28
Marwan Burelle <marwan@lse.epita.fr>
2017-07-04 15:46:49 +0200

    Python bindings done, need testing.
    The bindings are set-up, basic tests are ok.
    
    What to do:
    
    * Extends error handling and error messages
    * Better checking of input
    * Tests, lot of tests

--- python/spanner_clustering_py.cc
+++ python/spanner_clustering_py.cc
@@ -93,7 +93,7 @@ graph_out build_graph(size_t dim, std::vector<sample>& points, double stretch)
 }
 
 /*
- * Python bindings
+ * Translation unit between C++ and python
  */
 
 /*
@@ -106,13 +106,8 @@ struct SpannerGraph {
   PyObject *number_of_clusters;
 };
 
-static
-PyObject* SpannerGraph_new(PyTypeObject *typ, PyObject *args, PyObject *kwds)
-{
-  SpannerGraph self;
-
-  return typ->tp_alloc(typ, 0);
-}
+/* my error */
+static PyObject *spanner_graph_error;
 
 static
 sample build_sample(PyObject *smpl, size_t dim)
@@ -141,7 +136,7 @@ bool check_points_list(PyObject *py_points, size_t dim)
     size_t len = PySequence_Size(py_points);
     for (size_t i = 0; i < len; i++) {
       auto item = PySequence_GetItem(py_points, i);
-      if (!PySequence_Check(item) || PySequence_Size(item) != dim)
+      if (!PySequence_Check(item) || PySequence_Size(item) != (Py_ssize_t)dim)
         return false;
       for (size_t j = 0; j < dim; j++) {
         if (!PyFloat_Check(PySequence_GetItem(item, j)))
@@ -154,20 +149,183 @@ bool check_points_list(PyObject *py_points, size_t dim)
 }
 
 static
-int SpannerGraph_init(SpannerGraph *self, PyObject *args, PyObject *kwds)
+bool generate_py_object(SpannerGraph* self, graph_out& g)
+{
+
+  /*
+   * creating/allocating base data
+   * XXX: maybe some place for exceptions, also need to check for refcount
+   * decrement ...
+   */
+  self->edges = PyList_New(0);
+  if (!self->edges)
+    return false;
+
+  self->membership = PyList_New(g.membership.size());
+  if (!self->membership)
+    return false;
+
+  self->number_of_clusters = PyLong_FromSize_t(g.number_of_clusters);
+  if (!self->number_of_clusters)
+    return false;
+
+  /*
+   * Now we can translate the 2 vectors
+   */
+
+  /*
+   * First, the membership
+   */
+  for (size_t i = 0; i < g.membership.size(); i++) {
+    auto n = PyLong_FromSize_t(g.membership[i]);
+    if (!n)
+      return false;
+    PyList_SET_ITEM(self->membership, i, n);
+  }
+
+  /*
+   * Now edges
+   */
+  for (const auto& e : g.edges) {
+    auto item = Py_BuildValue("IId", e.src, e.dst, e.dist);
+    if (!item)
+      return false;
+    if (PyList_Append(self->edges, item) == -1)
+      return false;
+  }
+
+  return true;
+}
+
+
+/*
+ * Python bindings
+ */
+
+static
+PyObject* SpannerGraph_new(PyTypeObject *typ, PyObject*, PyObject*)
+{
+  return typ->tp_alloc(typ, 0);
+}
+
+static
+int SpannerGraph_init(SpannerGraph *self, PyObject *args, PyObject*)
 {
   unsigned long dim;
   double stretch ;
   PyObject *py_points;
-  if (!PyArg_ParseTuple(args, "kOd", &dim, &py_points, &stretch))
+  if (!PyArg_ParseTuple(args, "kOd", &dim, &py_points, &stretch)) {
     return -1;
+  }
 
-  if (!check_points_list(py_points, dim))
+  if (!check_points_list(py_points, dim)) {
+    PyErr_SetString(spanner_graph_error, "Wrong parameters form");
     return -1;
+  }
 
   auto points = build_points(py_points, dim);
 
   auto g = build_graph(dim, points, stretch);
 
+  if (!generate_py_object(self, g)) {
+    PyErr_SetString(spanner_graph_error, "Error while translating graph object");
+    return -1;
+  }
+
   return 0;
+}
+
+static
+void SpannerGraph_dealloc(SpannerGraph* self)
+{
+  Py_XDECREF(self->edges);
+  Py_XDECREF(self->membership);
+  Py_XDECREF(self->number_of_clusters);
+  Py_TYPE(self)->tp_free((PyObject*)self);
+}
+
+static
+PyMemberDef SpannerGraph_members[] = {
+  {(char*)"edges", T_OBJECT_EX, offsetof(SpannerGraph, edges), 0, (char*)"list of edges"},
+  {
+    (char*)"membership", T_OBJECT_EX, offsetof(SpannerGraph, membership), 0,
+    (char*)"cluster membership"
+  },
+  {
+    (char*)"number_of_clusters", T_OBJECT_EX,
+    offsetof(SpannerGraph, number_of_clusters),
+    0, (char*)"number of clusters"
+  },
+  {0,0,0,0,0}
+};
+
+static PyTypeObject SpannerGraphType= {
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "spanner_graph.SpannerGraph",             /* tp_name */
+    sizeof(SpannerGraph),             /* tp_basicsize */
+    0,                         /* tp_itemsize */
+    (destructor)SpannerGraph_dealloc, /* tp_dealloc */
+    0,                         /* tp_print */
+    0,                         /* tp_getattr */
+    0,                         /* tp_setattr */
+    0,                         /* tp_reserved */
+    0,                         /* tp_repr */
+    0,                         /* tp_as_number */
+    0,                         /* tp_as_sequence */
+    0,                         /* tp_as_mapping */
+    0,                         /* tp_hash  */
+    0,                         /* tp_call */
+    0,                         /* tp_str */
+    0,                         /* tp_getattro */
+    0,                         /* tp_setattro */
+    0,                         /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT |
+        Py_TPFLAGS_BASETYPE,   /* tp_flags */
+    "Spanner Graph Object",           /* tp_doc */
+    0,                         /* tp_traverse */
+    0,                         /* tp_clear */
+    0,                         /* tp_richcompare */
+    0,                         /* tp_weaklistoffset */
+    0,                         /* tp_iter */
+    0,                         /* tp_iternext */
+    0,             /* tp_methods */
+    SpannerGraph_members,             /* tp_members */
+    0,                         /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)SpannerGraph_init,      /* tp_init */
+    0,                         /* tp_alloc */
+    SpannerGraph_new,                 /* tp_new */
+};
+
+static PyModuleDef spanner_graph_module = {
+  PyModuleDef_HEAD_INIT,
+  "spanner_graph",
+  "Geometric Spanner and clusters.",
+  -1,
+  0, 0, 0, 0, 0
+};
+
+PyMODINIT_FUNC
+PyInit_spanner_graph(void)
+{
+  PyObject *m;
+  if (PyType_Ready(&SpannerGraphType) < 0)
+    return NULL;
+
+  m = PyModule_Create(&spanner_graph_module);
+  if (!m)
+    return NULL;
+
+  spanner_graph_error = PyErr_NewException("spanner_graph.error", NULL, NULL);
+  Py_INCREF(spanner_graph_error);
+  PyModule_AddObject(m, "error", spanner_graph_error);
+
+  Py_INCREF(&SpannerGraphType);
+  PyModule_AddObject(m, "SpannerGraph", (PyObject*)&SpannerGraphType);
+
+  return m;
 }
